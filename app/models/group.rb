5e42e8c4977553b89cc35501bdc0dad650d9017e
kyoji-hirano <k-hirano@r-learning.co.jp>
2020-07-23 14:42:47 +0900

    Book-Group associations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   has_one_attached :image
   
+  has_many :books
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
   validates :name, presence: true, uniqueness: true  
